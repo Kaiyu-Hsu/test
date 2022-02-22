9d8fe15362aa7c2ab74a9938cb733f3f21e1c658
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-02-22 18:04:00 +0800

    update: Home &  Main page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>hi</h1>
+  <v-main>
+    <v-container fluid>
+      <v-row>
+        <v-col class="d-flex flex-column justify-center align-center">
+          <v-btn color="primary" elevation="2" @click="showUp"
+            >按下去會有驚喜</v-btn
+          >
+          <!-- max-width="250" -->
+          <v-img
+            v-if="surprised"
+            class="mt-5"
+            src="../../public/nothing.jpg"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
+
+<script>
+export default {
+  name: "Main",
+  data() {
+    return {
+      surprised: false,
+    };
+  },
+  methods: {
+    showUp() {
+      this.surprised = !this.surprised;
+    },
+  },
+};
+</script>

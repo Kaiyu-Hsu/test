bc5e8a94f703ca0dd03d87c40d8c3f894eb591fd
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-03-10 11:43:48 +0800

    add: Nav

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
 <template>
-  <v-app>
-    <!-- app color="primary" dark -->
-    <v-app-bar app color="blue" elevation="4">
-      <v-app-bar-nav-icon class="d-flex d-sm-none" @click="nav = true">
-      </v-app-bar-nav-icon>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="../public/peppa pig.jpg"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <span class="mr-2">吃什麼</span>
-        <v-spacer></v-spacer>
-        <v-tabs class="d-none d-sm-flex">
-          <v-tab v-for="(item, i) in listItems" :key="i">{{ item }}</v-tab>
-        </v-tabs>
-      </div>
-    </v-app-bar>
-
-    <!-- Add a navigation bar -->
-    <v-navigation-drawer v-model="nav" absolute temporary>
-      <v-list>
-        <v-list-item-group>
-          <v-list-item v-for="(item, i) in listItems" :key="i">{{
-            item
-          }}</v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
+  <v-app id="app">
+    <Nav />
     <!-- 根據 tabs 切換底下內容 -->
     <v-main>
       <router-view />
@@ -41,13 +9,14 @@
 </template>
 
 <script>
+import Nav from "./components/Nav.vue";
 export default {
   name: "App",
+  components: {
+    Nav,
+  },
   data() {
-    return {
-      listItems: ["1", "2", "3"],
-      nav: false,
-    };
+    return {};
   },
   methods: {},
 };
---
+++ src/components/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-app-bar app color="primary" elevation="4" dense>
+      <v-app-bar-nav-icon class="d-flex d-sm-none" @click="nav = true">
+      </v-app-bar-nav-icon>
+      <v-container class="d-flex justify-center align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="../../public/peppa pig.jpg"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h3 class="mr-4 text-no-wrap">吃什麼</h3>
+        <v-spacer></v-spacer>
+        <!-- tabs v-slot:extension-->
+        <template>
+          <v-tabs class="d-none d-sm-flex" background-color="primary">
+            <v-tabs-slider color="white"></v-tabs-slider>
+            <v-tab v-for="(item, i) in listItems" :key="i">{{ item }}</v-tab>
+          </v-tabs>
+        </template>
+      </v-container>
+    </v-app-bar>
+
+    <!-- navigation drawer -->
+    <v-navigation-drawer v-model="nav" absolute temporary>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item v-for="(item, index) in listItems" :key="index">
+            {{ item }}
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  data() {
+    return {
+      listItems: ["飲料", "每一餐", "宵夜"],
+      nav: false,
+    };
+  },
+  methods: {},
+};
+</script>

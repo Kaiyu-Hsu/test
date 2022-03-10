02c3e5359ae85a477c1bdd9bb818e4d2a8fd4de3
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-03-10 14:47:00 +0800

    add: routes & link about Beverage、Meal、NightSnack

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,22 +4,31 @@
       <v-app-bar-nav-icon class="d-flex d-sm-none" @click="nav = true">
       </v-app-bar-nav-icon>
       <v-container class="d-flex justify-center align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="../../public/peppa pig.jpg"
-          transition="scale-transition"
-          width="40"
-        />
+        <router-link
+          to="/"
+          style="text-decoration: none"
+          class="d-flex justify-center align-center"
+        >
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="../../public/peppa pig.jpg"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <h3 class="mr-4 text-no-wrap">吃什麼</h3>
+          <h3 class="mr-4 text-no-wrap">吃什麼</h3>
+        </router-link>
+
         <v-spacer></v-spacer>
-        <!-- tabs v-slot:extension-->
+        <!-- tabs -->
         <template>
           <v-tabs class="d-none d-sm-flex" background-color="primary">
             <v-tabs-slider color="white"></v-tabs-slider>
-            <v-tab v-for="(item, i) in listItems" :key="i">{{ item }}</v-tab>
+            <v-tab v-for="(item, i) in items" :key="i" :to="item.path">{{
+              item.title
+            }}</v-tab>
           </v-tabs>
         </template>
       </v-container>
@@ -29,8 +38,8 @@
     <v-navigation-drawer v-model="nav" absolute temporary>
       <v-list>
         <v-list-item-group>
-          <v-list-item v-for="(item, index) in listItems" :key="index">
-            {{ item }}
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.path">
+            {{ item.title }}
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -43,10 +52,29 @@ export default {
   name: "Nav",
   data() {
     return {
-      listItems: ["飲料", "每一餐", "宵夜"],
+      items: [
+        {
+          title: "飲料",
+          path: "/beverage",
+        },
+        {
+          title: "每一餐",
+          path: "/meal",
+        },
+        {
+          title: "宵夜",
+          path: "/nightSnack",
+        },
+      ],
       nav: false,
     };
   },
   methods: {},
 };
 </script>
+
+<style scoped>
+h3 {
+  color: black;
+}
+</style>
---
+++ src/components/NightSnack.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1>宵夜呢?</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "NightSnack",
+
+  data() {
+    return {};
+  },
+};
+</script>

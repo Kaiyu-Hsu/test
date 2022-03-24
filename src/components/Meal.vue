f7b2a8f308fe26d850071391bbd7f50334473a12
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-03-24 18:21:55 +0800

    update: try to put an image on the page

--- src/components/Meal.vue
+++ src/components/Meal.vue
@@ -1,15 +1,22 @@
 <template>
   <v-container>
     <h1>現在要吃什麼?</h1>
+    <v-divider class="mb-3"></v-divider>
+    <!-- <span>{{ list[0].url }}</span> -->
+    <v-img :src="list[0].download_url"></v-img>
   </v-container>
 </template>
 
 <script>
+import randomPic from "../json/randomPicture";
+
 export default {
   name: "Meal",
 
   data() {
-    return {};
+    return {
+      list: randomPic,
+    };
   },
 };
 </script>
